6644a34a9f512291e992b46d086e6bccfefbfb7e
蒋伟东 <weidong17@satff.weibo.com>
2022-03-02 23:39:06 +0800

    pod init

---
+++ WDChat/Pods/Target Support Files/Pods-WDChat-WDChatUITests/Pods-WDChat-WDChatUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WDChat_WDChatUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WDChat_WDChatUITests
+@end
---
+++ WDChat/Pods/Target Support Files/Pods-WDChat/Pods-WDChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WDChat : NSObject
+@end
+@implementation PodsDummy_Pods_WDChat
+@end
